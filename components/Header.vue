4d6ddaa3e695984615cf78202c94cf79fcf9979a
Noor Rehman <786uog@gmail.com>
2024-10-17 19:02:37 +0500

    Logo and Logo Title size changed again

--- components/Header.vue
+++ components/Header.vue
@@ -151,7 +151,7 @@
             <path
                 d="M21 16v-2l-8-5V3.5c0-.83-.67-1.5-1.5-1.5S10 2.67 10 3.5V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5l8 2.5z"/>
           </svg>
-          <span class="ml-1 md:text-xl text-lg font-semibold" @click="hideDropdown">Find Cheap Booking</span>
+          <span class="md:ml-1 md:text-xl text-lg font-bold" @click="hideDropdown">Find Cheap Booking</span>
         </div>
       </nuxt-link>
     </div>
